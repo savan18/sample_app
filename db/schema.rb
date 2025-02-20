2ef6e5508afdf71d9eed0287e1fabeb53f50927c
Savan Hirpara <savanhirpara48@gmail.com>
2025-02-20 07:19:07 +0530

    pswd hash and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_24_080121) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_19_010602) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 end
